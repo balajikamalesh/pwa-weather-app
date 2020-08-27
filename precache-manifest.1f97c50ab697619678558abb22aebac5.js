d171a4b644a0e37c490ee161ce8cfe59e4e33f24
Balaji <abalajikamalesh@gmail.com>
2020-08-27 18:50:51 +0530

    Updates

---
+++ precache-manifest.1f97c50ab697619678558abb22aebac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd85436aa79c41fc7015a8927e5b02c3",
+    "url": "/pwa-weather-app/index.html"
+  },
+  {
+    "revision": "47c2422e54d34812b113",
+    "url": "/pwa-weather-app/static/css/main.7c1ed71c.chunk.css"
+  },
+  {
+    "revision": "2aaa2263f4ee5b885610",
+    "url": "/pwa-weather-app/static/js/2.d1f9fce3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pwa-weather-app/static/js/2.d1f9fce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c2422e54d34812b113",
+    "url": "/pwa-weather-app/static/js/main.1ad9f835.chunk.js"
+  },
+  {
+    "revision": "693578151ac021144c72",
+    "url": "/pwa-weather-app/static/js/runtime-main.7ccc10a2.js"
+  }
+]);
